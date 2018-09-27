8a45a567523358ddd5f9082dd0e61eb14fd41be8
Ahmad Shahwaiz <shahwaiz90@yahoo.com>
2018-09-27 10:28:14 +0500

    Fixed bugs.

--- app/src/main/java/io/smd/listview/ui/MainActivity.java
+++ app/src/main/java/io/smd/listview/ui/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Player> userArrayList;
     List<String> userList;
     private CustomAdapter userListViewAdapter;
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,39 +29,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView        =       findViewById(R.id.listView);
-        userArrayList   =       new ArrayList<Player>();
         userList        =       new ArrayList<String>();
 
+        //For ArrayAdapter
+        userList.add("Sarfaraz");
+        userList.add("Asif");
+        userList.add("Saad");
+        userList.add("Shadab");
+        userList.add("Amir");
+
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, userList);
+        listView.setAdapter(arrayAdapter);
+
+
+
+
         //For Custom Adapter
-        userArrayList.add(new Player("Fakhar Zaman", 204));
-        userArrayList.add(new Player("Imam", 22));
-        userArrayList.add(new Player("Babar", 22));
-        userArrayList.add(new Player("Sarfaraz", 26));
-        userArrayList.add(new Player("Shoaib Malik", 22));
-        userArrayList.add(new Player("Hassan Ali", 22));
-
-        //For Simple ArrayAdapter
-//        userList.add("Sarfaraz");
-//        userList.add("Asif");
-//        userList.add("Saad");
-//        userList.add("Shadab");
-//        userList.add("Amir");
-
-        //Attach Custom Adapter
-         userListViewAdapter = new CustomAdapter(userArrayList, this);
-        listView.setAdapter(userListViewAdapter);
-
-        
+//        userArrayList = new ArrayList<>();
+//        userArrayList.add(new Player("Fakhar Zaman", 204));
+//        userArrayList.add(new Player("Imam", 22));
+//        userArrayList.add(new Player("Babar", 22));
+//        userArrayList.add(new Player("Sarfaraz", 26));
+//        userArrayList.add(new Player("Shoaib Malik", 22));
+//        userArrayList.add(new Player("Hassan Ali", 22));
+//
+////        Attach Custom Adapter
+//         userListViewAdapter = new CustomAdapter(userArrayList, this);
+//        listView.setAdapter(userListViewAdapter);
+//
+//
 //        userListViewAdapter.notifyDataSetChanged();
-        //Attach ArrayAdapter
-//        ArrayAdapter<Player> arrayAdapter = new ArrayAdapter<Player>(this,android.R.layout.simple_list_item_1, userArrayList);
-//        listView.setAdapter(arrayAdapter);
 
     }
 
     public void AddPlayer(View view) {
 
-        userArrayList.add(new Player("Ahmad Shahwaiz", 22));
-        userListViewAdapter.notifyDataSetChanged();
+        //For ArrayAdapter
+        userList.add("Ahmad Shahzad");
+        arrayAdapter.notifyDataSetChanged();
+        //For Custom Adapter
+//        userArrayList.add(new Player("Ahmad Shahwaiz", 22));
+//        userListViewAdapter.notifyDataSetChanged();
     }
 }
